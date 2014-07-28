18be63052f52b8133ab6a709ac0cdb005d59608c
Hamish Downer <hamish@aptivate.org>
2014-07-28 11:57:55 +0100

    Make the root svg element in trademapper, not mapper
    so it can be reused

--- trademapper/js/trademapper.js
+++ trademapper/js/trademapper.js
@@ -5,9 +5,20 @@ define(["trademapper.mapper", "d3"], function(mapper, d3) {
 	var config, rootElement,
 
 	init = function(element, tmConfig) {
-		config = tmConfig;
+		config = tmConfig || {};
 		rootElement = element;
-		mapper.init(element, {ratio: 1.0});
+
+		config.ratio = config.ratio || 1.0;
+		config.width = parseInt(element.style('width'));
+		config.height = config.width * config.ratio;
+
+		tmsvg = element.insert("svg")
+			.attr("width", config.width)
+			.attr("height", config.height)
+			.attr("id", "mapcanvas")
+			.attr("class", "map-svg flow")
+			.attr("viewBox", "0 0 " + config.width + " " + config.height);
+		mapper.init(tmsvg, config);
 	};
 
 	return {
--- trademapper/js/trademapper.mapper.js
+++ trademapper/js/trademapper.mapper.js
@@ -1,24 +1,19 @@
 
 define(["d3", "topojson", "worldmap"], function(d3, topojson, mapdata) {
-	var mapsvg, mapWidth, mapHeight, config, pathmaker,
+	var mapsvg, config,
+		countries, borders,
+		pathmaker,
 
-	init = function(element, mapConfig) {
-		mapWidth = parseInt(element.style('width'));
-		mapHeight = mapWidth * mapConfig.ratio;
+	init = function(svgElement, mapConfig) {
+		mapsvg = svgElement;
 
 		var projection = d3.geo.mercator();
 			//.scale(mapWidth/1.25)
 			//.translate([mapWidth/4, mapHeight/2+10]);
 		pathmaker = d3.geo.path().projection(projection);
-		mapsvg = element.insert("svg")
-			.attr("width", mapWidth)
-			.attr("height", mapHeight)
-			.attr("id", "mapcanvas")
-			.attr("class", "map-svg flow")
-			.attr("viewBox", "0 0 " + mapWidth + " " + mapHeight);
 
-		var countries = topojson.feature(mapdata, mapdata.objects.subunits).features;
-		var borders = topojson.mesh(mapdata, mapdata.objects.subunits,
+		countries = topojson.feature(mapdata, mapdata.objects.subunits).features;
+		borders = topojson.mesh(mapdata, mapdata.objects.subunits,
 			function(a, b) { return true; });
 
 		mapsvg.selectAll(".subunit")

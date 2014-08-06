8bd318043aaddac81515e99fdfd47ca853703284
Hamish Downer <hamish@aptivate.org>
2014-08-06 17:40:15 +0100

    Make tests pass consistently

--- tests/js/test-route.js
+++ tests/js/test-route.js
@@ -13,11 +13,12 @@ define(
 			});
 
 			q.test('check PointLatLong sets point using latLongToPointFunc', function() {
+				route.setLatLongToPointFunc(function() { return [3, 4]; });
 				var point = new route.PointLatLong(5, 6);
-				q.equal(5, point.latlong[0], 'The x of latlong should be 5');
-				q.equal(6, point.latlong[1], 'The y of latlong should be 6');
-				q.equal(3, point.point[0], 'The x of point should be 3');
-				q.equal(4, point.point[1], 'The y of point should be 4');
+				q.equal(point.latlong[0], 5, 'The x of latlong should be 5');
+				q.equal(point.latlong[1], 6, 'The y of latlong should be 6');
+				q.equal(point.point[0], 3, 'The x of point should be 3');
+				q.equal(point.point[1], 4, 'The y of point should be 4');
 			});
 
 			q.test('check PointLatLong toString() includes lat and long', function() {

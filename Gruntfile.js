d3af2841f4852551134c19b6952d8946f01ee2a1
leanne <leanne.tite@gmail.com>
2014-11-08 15:54:57 +0000

    Grunt copy file with resources and build folders for images

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,47 @@
 module.exports = function(grunt) {
-    var sourceDir = 'trademapper/resources',
-        buildDir = 'trademapper/build';
+    var sourceDir = 'trademapper/resources/',
+        buildDir = 'trademapper/build/';
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         
+        //Compile sass to css
         sass: {
             dist: {
-                src: sourceDir + '/styles/**/*.scss',
-                dest: buildDir + '/css/trademapper.css'
+                src: sourceDir + 'styles/**/*.scss',
+                dest: buildDir + 'css/trademapper.css'
             } 
         },
 
+        //Watch files for changes, then run given task
         watch: {
             sass: {
-                files: [sourceDir + '/styles/**/*.scss'],
+                files: [sourceDir + 'styles/**/*.scss'],
                 tasks: ['sass']
             } 
         },
+        
+        //Copy source files to build dir
+        copy: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: sourceDir,
+                    src: [
+                        'images/**/*.jpg',
+                        'images/**/*.png',
+                        'images/**/*.gif',
+                        'images/**/*.svg'
+                    ],
+                    dest: buildDir
+                }]
+            },
+        }        
     });
 
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default',['watch']);
+    grunt.registerTask('default',['copy', 'watch']);
 }

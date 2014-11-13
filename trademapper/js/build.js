3391e9f8af0657ea2a2ebea77c800103e09fa004
Hamish Downer <hamish@aptivate.org>
2014-11-13 15:28:07 +0000

    refactor where the library js lives

--- trademapper/js/build.js
+++ trademapper/js/build.js
@@ -1,9 +1,17 @@
 ({
     baseUrl: ".",
     paths: {
-        d3: "d3/d3",
-        topojson: "d3/topojson.v1",
-        queue: "d3/queue.v1",
+		// require.js bits
+		domReady: "lib/domReady",
+		text: "lib/text",
+		// d3 bits
+		d3: "lib/d3",
+		queue: "lib/queue.v1",
+		topojson: "lib/topojson.v1",
+		// map data - jsons imported by require
+		countrycentre: "map/countrycentre",
+		disputedareas: "map/disputedareas",
+		worldmap: "map/worldmap"
     },
     name: "main",
     out: "main-built.js",
--- trademapper/js/main.js
+++ trademapper/js/main.js
@@ -1,8 +1,17 @@
 require.config({
 	baseUrl: 'js',
 	paths: {
-		d3: "d3/d3",
-		topojson: "d3/topojson.v1"
+		// require.js bits
+		domReady: "lib/domReady",
+		text: "lib/text",
+		// d3 bits
+		d3: "lib/d3",
+		queue: "lib/queue.v1",
+		topojson: "lib/topojson.v1",
+		// map data - jsons imported by require
+		countrycentre: "map/countrycentre",
+		disputedareas: "map/disputedareas",
+		worldmap: "map/worldmap"
 	}
 });
 

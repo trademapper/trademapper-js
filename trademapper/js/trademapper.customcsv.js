2ce46abea83cce4e01839090ebebb032434b113c
Hamish Downer <hamish@aptivate.org>
2014-11-19 15:17:55 +0000

    customcsv: make indentation consistent

--- trademapper/js/trademapper.customcsv.js
+++ trademapper/js/trademapper.customcsv.js
@@ -9,81 +9,81 @@ define([
 ) {
 
 	return {
-	formProcessedCallback: null,
+		formProcessedCallback: null,
 
-	typeSelectConfig: {
-		type: 'type',
-		options: [
-			{ text: 'Ignore',   value: 'ignore' },
-			{ text: 'Location', value: 'location'},
-			{ text: 'Location (extra info)', value: 'location_extra'},
-			{ text: 'Quantity', value: 'quantity' },
-			{ text: 'Text',     value: 'text' },
-			{ text: 'Text List', value: 'text_list' },
-			{ text: 'Year',     value: 'year' }
-		]
-	},
-	locationTypeSelectConfig: {
-		type: 'locationType',
-		options: [
-			{ text: 'Country Code', value: 'country_code' },
-			{ text: 'Country Code List', value: 'country_code_list' },
-			{ text: 'Latitude/Longitude Name', value: 'latLongName' }
-		]
-	},
-	locationExtraTypeSelectConfig: {
-		type: 'locationExtraType',
-		options: [
-			{ text: 'Latitude', value: 'longitude' },
-			{ text: 'Longitude', value: 'latitude' }
-		]
-	},
-	locationRoleSelectConfig: {
-		type: 'locationRole',
-		options: [
-			{ text: 'Exporter', value: 'exporter' },
-			{ text: 'Importer', value: 'importer' },
-			{ text: 'Origin',   value: 'origin' },
-			{ text: 'Transit',  value: 'transit' }
-		]
-	},
-	locationOrderTextConfig: {
-		type: 'locationOrder',
-		label: 'Location Order',
-		maxlength: 3,
-		title: 'A number to allow ordering of the locations'
-	},
-	shortNameTextConfig: {
-		type: 'shortName',
-		label: 'Alt Name',
-		title: 'An optional name to display instead of the column header'
-	},
-	isUnitCheckboxConfig: {
-		type: 'isUnit',
-		label: 'Is "units" column',
-		title: 'Whether this column is the units (only one column can have that value).'
-	},
-	multiSelectCheckboxConfig: {
-		type: 'multiSelect',
-		label: 'multi-select',
-		title: 'Can multiple options be selected in the filters.'
-	},
-	// TODO:
-	// - text: verboseNames !!!
+		typeSelectConfig: {
+			type: 'type',
+			options: [
+				{ text: 'Ignore',   value: 'ignore' },
+				{ text: 'Location', value: 'location'},
+				{ text: 'Location (extra info)', value: 'location_extra'},
+				{ text: 'Quantity', value: 'quantity' },
+				{ text: 'Text',     value: 'text' },
+				{ text: 'Text List', value: 'text_list' },
+				{ text: 'Year',     value: 'year' }
+			]
+		},
+		locationTypeSelectConfig: {
+			type: 'locationType',
+			options: [
+				{ text: 'Country Code', value: 'country_code' },
+				{ text: 'Country Code List', value: 'country_code_list' },
+				{ text: 'Latitude/Longitude Name', value: 'latLongName' }
+			]
+		},
+		locationExtraTypeSelectConfig: {
+			type: 'locationExtraType',
+			options: [
+				{ text: 'Latitude', value: 'longitude' },
+				{ text: 'Longitude', value: 'latitude' }
+			]
+		},
+		locationRoleSelectConfig: {
+			type: 'locationRole',
+			options: [
+				{ text: 'Exporter', value: 'exporter' },
+				{ text: 'Importer', value: 'importer' },
+				{ text: 'Origin',   value: 'origin' },
+				{ text: 'Transit',  value: 'transit' }
+			]
+		},
+		locationOrderTextConfig: {
+			type: 'locationOrder',
+			label: 'Location Order',
+			maxlength: 3,
+			title: 'A number to allow ordering of the locations'
+		},
+		shortNameTextConfig: {
+			type: 'shortName',
+			label: 'Alt Name',
+			title: 'An optional name to display instead of the column header'
+		},
+		isUnitCheckboxConfig: {
+			type: 'isUnit',
+			label: 'Is "units" column',
+			title: 'Whether this column is the units (only one column can have that value).'
+		},
+		multiSelectCheckboxConfig: {
+			type: 'multiSelect',
+			label: 'multi-select',
+			title: 'Can multiple options be selected in the filters.'
+		},
+		// TODO:
+		// - text: verboseNames !!!
 
-	detectColumnType: function(val) {
-		var asInt = parseInt(val, 10);
-		if (!isNaN(asInt) && asInt > 1900 && asInt < 2100) {
-			return 'year';
-		}
-		else if (!isNaN(parseFloat(val, 10))) {
-			return 'quantity';
-		} else if (val.match(/^[A-Z]{2}$/)) {
-			return 'location';
-		} else {
-			return 'ignore';
-		}
-	},
+		detectColumnType: function(val) {
+			var asInt = parseInt(val, 10);
+			if (!isNaN(asInt) && asInt > 1900 && asInt < 2100) {
+				return 'year';
+			}
+			else if (!isNaN(parseFloat(val, 10))) {
+				return 'quantity';
+			} else if (val.match(/^[A-Z]{2}$/)) {
+				return 'location';
+			} else {
+				return 'ignore';
+			}
+		},
 
 		init: function(rawCsv, callback) {
 			// use d3 csv parsing

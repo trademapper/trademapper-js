789379ee8df15d6649b67dbb86795d20df2f9085
Elliot Smith <elliots@aptivate.org>
2018-11-15 15:49:25 +0000

    Export image as SVG
    This greatly simplifies and speeds up the process of exporting
    the image, and also works in Firefox.
    
    While SVG may be a less familiar format than PNG or JPG, it's
    very easy to convert to those other formats if required.

--- trademapper/js/trademapper.imageexport.js
+++ trademapper/js/trademapper.imageexport.js
@@ -12,12 +12,8 @@ function($) {
 		/**
 		 * Constructor
 		 *
-		 * This creates an off-screen canvas for drawing the SVG to, which is wholly
-		 * managed by this component, never added to the DOM, and never visible to
-		 * the user.
-		 *
-     * It also creates a hidden link which allows a download to be emulated,
-		 * and an Image object for loading the SVG into.
+     * This creates a hidden link which allows a download to be emulated
+		 * by settings its href attribute to a dataURL extracted from the map SVG.
 		 *
 		 * button: element with an on() method which can be used to bind clicks
 		 * svgElement: DOM node; reference to the <svg> element containing the map
@@ -27,33 +23,21 @@ function($) {
 			button.on('click', this.exportSvg.bind(this));
 
 			this.svgElement = $(svgElement);
-			this.canvas = document.createElement("canvas");
-			this.ctx = this.canvas.getContext("2d");
 
 			this.link = document.createElement("a");
-			this.image = new Image();
+			this.link.style.display = "none";
+			this.link.download = "map.svg";
+			document.body.appendChild(this.link);
 		},
 
 		// export the associated SVG element
 		exportSvg: function() {
-			var self = this;
-
-			this.canvas.height = this.svgElement.height();
-			this.canvas.width = this.svgElement.width();
-
-			this.image.onload = function() {
-				self.ctx.drawImage(self.image, 0, 0);
-
-				self.link.download = "sample.png";
-				self.link.href = self.canvas.toDataURL("image/png");
-				self.link.click();
-			};
-
 			this.svgElement.attr("version", 1.1);
 			this.svgElement.attr("xmlns", "http://www.w3.org/2000/svg");
 
 			var svgString = btoa(this.svgElement.get(0).outerHTML);
-			this.image.src = DATA_URL_HEADER + svgString;
+			this.link.href = DATA_URL_HEADER + svgString;
+			this.link.click();
 		}
 	};
 });

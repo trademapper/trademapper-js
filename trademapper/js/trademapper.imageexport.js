43b71c88d68fa3ee503611fdcd0cbab3a3714277
Elliot Smith <elliots@aptivate.org>
2018-11-16 09:44:25 +0000

    Use Blob to export image to SVG

--- trademapper/js/trademapper.imageexport.js
+++ trademapper/js/trademapper.imageexport.js
@@ -3,8 +3,6 @@ define([
 ],
 
 function($) {
-	const DATA_URL_HEADER = "data:image/svg+xml;base64,";
-
 	return {
 		/**
 		 * Constructor
@@ -17,13 +15,16 @@ function($) {
 		 */
 		init: function(button, svgElement) {
 			// clicks on the button export the SVG
-			button.on('click', this.exportSvg.bind(this));
+			button.on("click", this.exportSvg.bind(this));
 
 			this.svgElement = $(svgElement);
 
 			this.link = document.createElement("a");
 			this.link.style.display = "none";
+
+			// name given to file when downloaded
 			this.link.download = "map.svg";
+
 			document.body.appendChild(this.link);
 		},
 
@@ -32,8 +33,10 @@ function($) {
 			this.svgElement.attr("version", 1.1);
 			this.svgElement.attr("xmlns", "http://www.w3.org/2000/svg");
 
-			var svgString = btoa(this.svgElement.get(0).outerHTML);
-			this.link.href = DATA_URL_HEADER + svgString;
+			var svgString = this.svgElement.get(0).outerHTML;
+			var blob = new Blob([svgString], {type: "image/svg+xml"});
+
+			this.link.href = window.URL.createObjectURL(blob);
 			this.link.click();
 		}
 	};

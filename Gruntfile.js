790bf048fae871237978580c30ca384a2eface55
Elliot Smith <elliots@aptivate.org>
2018-12-18 11:52:48 +0000

    Copy configextra.default.js to dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,50 +1,50 @@
 module.exports = function(grunt) {
-    var sourceDir = 'trademapper',
+		var sourceDir = 'trademapper',
 				resourceDir = sourceDir + '/resources/',
-        buildDir = sourceDir + '/build/',
+				buildDir = sourceDir + '/build/',
 				distDir = 'dist/';
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+		grunt.initConfig({
+				pkg: grunt.file.readJSON('package.json'),
 
-        //Compile sass to css
-        sass: {
-            dist: {
-                src: resourceDir + 'styles/**/*.scss',
-                dest: buildDir + 'css/trademapper.css'
-            }
-        },
+				//Compile sass to css
+				sass: {
+						dist: {
+								src: resourceDir + 'styles/**/*.scss',
+								dest: buildDir + 'css/trademapper.css'
+						}
+				},
 
-        //Watch files for changes, then run given task
-        watch: {
-            sass: {
-                files: [resourceDir + 'styles/**/*.scss'],
-                tasks: ['sass']
-            }
-        },
+				//Watch files for changes, then run given task
+				watch: {
+						sass: {
+								files: [resourceDir + 'styles/**/*.scss'],
+								tasks: ['sass']
+						}
+				},
 
 				clean: {
 						dist: [distDir]
 				},
 
-        //Copy source files to build dir or dist directory
-        copy: {
-            build: {
-                files: [{
-                    expand: true,
-                    cwd: resourceDir,
-                    src: [
-                        'images/**/*.jpg',
-                        'images/**/*.png',
-                        'images/**/*.gif',
-                        'images/**/*.svg',
-                        'fonts/*',
-                        'styles/*.css',
-                        'styles/*.css.map'
-                    ],
-                    dest: buildDir
-                }]
-            },
+				//Copy source files to build dir or dist directory
+				copy: {
+						build: {
+								files: [{
+										expand: true,
+										cwd: resourceDir,
+										src: [
+												'images/**/*.jpg',
+												'images/**/*.png',
+												'images/**/*.gif',
+												'images/**/*.svg',
+												'fonts/*',
+												'styles/*.css',
+												'styles/*.css.map'
+										],
+										dest: buildDir
+								}]
+						},
 
 						// this copies files from build/, so copy:build needs to be run
 						// first for this to produce a working distribution
@@ -54,29 +54,35 @@ module.exports = function(grunt) {
 												expand: true,
 												cwd: sourceDir,
 												src: [
-													'index.html',
-													'js/**',
-													'fragments/**',
 													'build/**',
-													'sample_data/**',
 													'!build/css/trademapper.css.map',
 													'!build/styles/bootstrap.css',
 													'!build/styles/bootstrap-switch.css',
 													'!build/styles/bootstrap.css.map',
+													'fragments/**',
+													'index.html',
+													'js/**',
+													'!js/configextra.js',
+													'!js/configextra.default.js',
+													'sample_data/**',
 												],
 												dest: distDir
 										},
+										{
+												src: sourceDir + '/js/configextra.default.js',
+												dest: distDir + '/js/configextra.js',
+										}
 								]
 						}
-        },
-    });
+				},
+		});
 
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+		grunt.loadNpmTasks('grunt-contrib-copy');
+		grunt.loadNpmTasks('grunt-contrib-sass');
+		grunt.loadNpmTasks('grunt-contrib-watch');
 		grunt.loadNpmTasks('grunt-contrib-clean');
 
-    grunt.registerTask('default',['sass', 'copy:build', 'watch']);
+		grunt.registerTask('default',['sass', 'copy:build', 'watch']);
 
 		// produces a built version of the app in the dist/ directory
 		grunt.registerTask('dist', ['clean:dist', 'sass', 'copy:build', 'copy:dist']);
